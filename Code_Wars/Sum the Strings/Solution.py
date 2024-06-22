8b72b1b463d0efb434287e476913f2fc8ce2ea86
Patryk <145399929+UmarlyPoeta@users.noreply.github.com>
2024-06-22 15:55:50 +0200

    completed sum the strings kata

---
+++ Code_Wars/Sum the Strings/Solution.py
@@ -0,0 +1,14 @@
+def sum_str(a, b):
+    # happy coding !
+    if a=="":
+        a=0
+    else:
+        a=int(a)
+    
+    if b=="":
+        b=0
+    else:
+        b=int(b)
+    
+    
+    return str(a+b)
